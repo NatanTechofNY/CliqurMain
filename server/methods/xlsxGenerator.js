719f0dfbe499a2ddff2935ebfbc6b59a6c1c742f
sum-kcid <jayp1924@gmail.com>
2016-04-05 16:42:49 -0400

    dlign

--- server/methods/xlsxGenerator.js
+++ server/methods/xlsxGenerator.js
@@ -16,6 +16,13 @@ if(Meteor.isServer) {
 					$('body tbody').append('<tr><td>'+g.fullName+'</td><td>'+g._id+'</td></tr>');
 				});
 
+				// userList = userList.map(function(g) {
+				// 	return [g._id, g.fullName];
+				// });
+
+				// var buildr = [{id: 1, name: ("List for " + moment().format('LL')), data: userList}];
+				
+				// var bfr = nxlsx.build(buildr);
 
 
 				return $.html();
