610d143ea95d8f4f9246316d9f7b8a2852d924a4
sum-kcid <jayp1924@gmail.com>
2016-04-13 19:44:56 -0400

    added contact form

--- server/accounts/mailgun.js
+++ server/accounts/mailgun.js
@@ -18,7 +18,35 @@ if (Meteor.isServer) {
 
 				from: 'Reports@cliqur.com',
 				subject: 'Attendance report for: ' + data.className,
-				html: data.reportHtml
+				html: sanitizeHtml(data.reportHtml, {
+					allowedTags: ['h1', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'p1', 'a', 'ul', 'ol',
+  'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+  'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre' , 'section', 'button'],
+  					allowedAttributes: {
+  						'*': ['style', 'id']
+  					}
+				})
+			});
+		},
+		sendContact: function (d) {
+			check([d.nm, d.em, d.ms], [String]);
+			if (d.ph) check([d.ph], [String]);
+			if (d.ms.length < 5) throw new Meteor.Error('Please write a longer message.')
+				else if (d.ms.length > 5000) throw new Meteor.Error('Max 5,000 chars for the message.');
+
+
+			var newhtml = d.ms.replace(/&/g, '&amp;').replace(/</g, '&#60;').replace(/>/g, '&#62;').replace(/\n\s*\n/g, '\n\n').replace(/\n/g, '<br />');
+			var tohtmlize = sanitizeHtml(newhtml, {
+				allowedTags: ['br'],
+					allowedAttributes: []
+			});
+			var htmlIn = '<div><strong>Email by: '+d.nm+' | Sender address: '+d.em+' | Phone: '+(d.ph? d.ph: 'no phone number given.')+'</strong><br />' + tohtmlize.toString() + '</div>';
+			Meteor.Mailgun.send({
+				to: 'cliqur.contact@gmail.com',
+
+				from: d.em,
+				subject: 'Contact Form | Filled by ' + d.nm,
+				html: htmlIn
 			});
 		}
 	});
