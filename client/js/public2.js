dd54d0dd3fe539885a5a8f851d4b68de0da70c38
NatanTechofNY <natan@ynatan.com>
2016-05-08 20:27:42 -0400

    Added full screen option for public view

--- client/js/public2.js
+++ client/js/public2.js
@@ -11,5 +11,28 @@ if (Meteor.isClient) {
             event.preventDefault();
         }
     },
+    'click #fullscreen': function() {
+
+  var el = document.body;
+
+  // Supports most browsers and their versions.
+  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen 
+  || el.mozRequestFullScreen || el.msRequestFullScreen;
+
+  if (requestMethod) {
+
+    // Native full screen.
+    requestMethod.call(el);
+
+  } else if (typeof window.ActiveXObject !== "undefined") {
+
+    // Older IE.
+    var wscript = new ActiveXObject("WScript.Shell");
+
+    if (wscript !== null) {
+      wscript.SendKeys("{F11}");
+    }
+  }
+},
 	});
 };
