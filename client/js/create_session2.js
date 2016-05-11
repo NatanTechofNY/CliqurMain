3da364cb0e0332a12612a3132edabf17654a7f83
NatanTechofNY <natan@ynatan.com>
2016-05-10 20:27:05 -0400

    All new files

--- client/js/create_session2.js
+++ client/js/create_session2.js
@@ -1,4 +1,83 @@
 if (Meteor.isClient) {
+  var pagingRows = 10;
+ 
+    var paginationOptions = {
+        innerWindow: 1,
+        left: 0,
+        right: 0
+    };
+    var options = {
+        valueNames: [ 'sortID', 'sortDesc'],
+        page: pagingRows,
+        plugins: [ ListPagination(paginationOptions) ],
+    };
+    var tableList = new List('tableID', options);
+ 
+    Template.create_session2.events({
+        'click .ses_custom-select-trigger': function (event) {
+            $('html')
+            .one('click',function() {
+                $(".ses_custom-select").removeClass("ses_opened");
+            });
+            $(event.currentTarget).parents(".ses_custom-select").toggleClass("ses_opened");
+            event.stopPropagation();
+        },
+        'click .ses_custom-option': function(event) {
+            $(event.currentTarget).parents(".ses_custom-select-wrapper").find("select").val($(event.currentTarget).data("value"));
+            $(event.currentTarget).parents(".ses_custom-options").find(".ses_custom-option").removeClass("ses_selection");
+            $(event.currentTarget).addClass("ses_selection");
+            $(event.currentTarget).parents(".ses_custom-select").removeClass("ses_opened");
+            $(event.currentTarget).parents(".ses_custom-select").find(".ses_custom-select-trigger").text($(event.currentTarget).text());
+        },
+        'click .ses_jTablePageNext': function() {
+            var list = $('.ses_pagination').find('li');
+            $.each(list, function(position, element){
+                if($(element).is('.ses_active')){
+                    $(list[position-1]).trigger('click');
+                };
+            });
+        }
+    });
+ 
+    var $t_forprivcall = this;
+    Template.create_session2.rendered = function () {
+        (function () {
+            var removeSuccess;
+            removeSuccess = function () {
+                return $('.ses_button').removeClass('ses_success');
+            };
+            $(document).ready(function () {
+                return $('.ses_button').click(function () {
+                $(this).addClass('ses_success');
+                    return setTimeout(removeSuccess, 3000);
+                });
+            });
+        }.call($t_forprivcall));
+ 
+        $(".ses_custom-select").each(function() {
+            var classes = $(this).attr("class"),
+                id      = $(this).attr("id"),
+                name    = $(this).attr("name");
+            var template =  '<div class="' + classes + '">';
+                template += '<span class="ses_custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+                template += '<div class="ses_custom-options">';
+                $(this).find("option").each(function() {
+                    template += '<span class="ses_custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+                });
+            template += '</div></div>';
+ 
+            $(this).wrap('<div class="ses_custom-select-wrapper"></div>');
+            $(this).hide();
+            $(this).after(template);
+        });
+ 
+ 
+        $(".ses_custom-option:first-of-type").hover(function() {
+            $(this).parents(".ses_custom-options").addClass("ses_option-hover");
+        }, function() {
+            $(this).parents(".ses_custom-options").removeClass("ses_option-hover");
+        });
+    };
   Blaze._allowJavascriptUrls();
 
   Template.create_session2.destroyed = function () {
